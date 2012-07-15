4224c9a84d3a906de24c35d3501c3fa08be8621f
Ben L <nightgunner5@llamaslayers.net>
2012-07-15 13:14:41 -0500

    Split readme into a separate file

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	. "llamaslayers.net/go.cms/document"
-	. "llamaslayers.net/go.cms/formatter"
-)
+import "flag"
 
 var flagReadme = flag.Bool("readme", false, "Prints a README and exits.")
 var flagReadmeFormat = flag.String("readme-format", "Markdown", "Other formats available are LaTeX and HTML.")
@@ -14,26 +9,7 @@ func main() {
 	flag.Parse()
 
 	if *flagReadme {
-		readme := &Document{
-			"go.cms Readme",
-			Content{
-				&Link{
-					"http://travis-ci.org/Nightgunner5/go.cms",
-					Content{&Image{"https://secure.travis-ci.org/Nightgunner5/go.cms.png?branch=master", "Build Status"}},
-				},
-			},
-		}
-
-		switch *flagReadmeFormat {
-		case "Markdown":
-			fmt.Println(Markdown.Format(readme))
-		case "LaTeX":
-			fmt.Println(LaTeX.Format(readme))
-		case "HTML":
-			fmt.Println(HTML.Format(readme))
-		default:
-			fmt.Println("Unknown readme-format value. Capitalization matters!")
-		}
+		readme()
 		return
 	}
 }
---
+++ readme.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	. "llamaslayers.net/go.cms/document"
+	. "llamaslayers.net/go.cms/formatter"
+)
+
+func readme() {
+		readme := &Document{
+			"go.cms",
+			Content{
+				&Link{
+					"http://travis-ci.org/Nightgunner5/go.cms",
+					Content{&Image{"https://secure.travis-ci.org/Nightgunner5/go.cms.png?branch=master", "Build Status"}},
+				},
+			},
+		}
+
+		switch *flagReadmeFormat {
+			case "Markdown":
+				fmt.Println(Markdown.Format(readme))
+			case "LaTeX":
+				fmt.Println(LaTeX.Format(readme))
+			case "HTML":
+				fmt.Println(HTML.Format(readme))
+			default:
+				fmt.Println("Unknown readme-format value. Capitalization matters!")
+		}
+	}
