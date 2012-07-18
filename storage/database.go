fd55cc92fa1e595c437e8652f886e572ca7fb0e5
Ben L <nightgunner5@llamaslayers.net>
2012-07-18 00:07:59 -0500

    Database connection caching

--- storage/database.go
+++ storage/database.go
@@ -10,15 +10,28 @@ import (
 	"unicode"
 )
 
-var db *sql.DB
+const NUM_DB_CONNECTIONS = 10
+
+var dbCache = make(chan *sql.DB, NUM_DB_CONNECTIONS)
+
+func acquire() *sql.DB {
+	return <-dbCache
+}
+func recycle(db *sql.DB) {
+	dbCache <- db
+}
 
 func Startup(loc string) {
-	var err error
-	db, err = sql.Open("sqlite3", loc)
-	if err != nil {
-		panic(err)
+	for i := 0; i < NUM_DB_CONNECTIONS; i++ {
+		db, err := sql.Open("sqlite3", loc)
+		if err != nil {
+			panic(err)
+		}
+		dbCache <- db
 	}
 
+	db := acquire()
+	defer recycle(db)
 	version, err := db.Query("SELECT revision FROM version WHERE module='database';")
 	if err != nil { // No database? No problem!
 		if err.Error() != "no such table: version" {
@@ -77,6 +90,8 @@ CREATE UNIQUE INDEX url ON articles (url);
 }
 
 func updateDBVersion(module string, version uint64) {
+	db := acquire()
+	defer recycle(db)
 	stmt, _ := db.Prepare("REPLACE INTO version VALUES(?, ?);")
 	stmt.Exec(module, version)
 	log.Printf("Updated %s to version %d.", module, version)
@@ -103,48 +118,66 @@ func SanitizeURL(text string) string {
 	return string(url)
 }
 
-func GetArticleIDByURL(url string) int64 {
-	stmt, _ := db.Prepare("SELECT id FROM articles WHERE url = ?;")
-	result, _ := stmt.Query(url)
+func GetArticleIDByURL(url string) (int64, error) {
+	db := acquire()
+	defer recycle(db)
+
+	result, err := db.Query("SELECT id FROM articles WHERE url = ?;", url)
+	if err != nil {
+		return 0, err
+	}
+	defer result.Close()
 
 	var id int64
 	for result.Next() {
 		result.Scan(&id)
 		// TODO: Cache this
 	}
-	return id
+	return id, nil
 }
 
-func InsertArticle(article *document.Article) int64 {
+func InsertArticle(article *document.Article) (int64, error) {
+	db := acquire()
+	defer recycle(db)
+
 	url := SanitizeURL(article.Title)
-	if GetArticleIDByURL(url) != 0 {
-		i := 2 // Post 1 already exists and post 0 would only exist if everyone were nerds.
-		for GetArticleIDByURL(fmt.Sprintf("%s-%d", url, i)) != 0 {
+	if id, _ := GetArticleIDByURL(url); id != 0 {
+		i := 1
+		for id != 0 {
 			i++
+			id, _ = GetArticleIDByURL(fmt.Sprintf("%s-%d", url, i))
 		}
 		url = fmt.Sprintf("%s-%d", url, i)
 	}
 
-	// I'm dropping the errors here because I'm tired. If someone can show me a case where this would actually
-	// error, I'll go back in and write error handling code.
-	blob, _ := FromElement(make([]byte, 0), article)
+	blob, err := FromElement(make([]byte, 0), article)
+	if err != nil {
+		return 0, err
+	}
 
-	stmt, _ := db.Prepare("INSERT INTO articles (title, timestamp, url, content) VALUES(?, ?, ?, ?);")
-	result, _ := stmt.Exec(article.Title, article.Timestamp.Unix(), url, blob)
-	id, _ := result.LastInsertId()
-	return id
+	result, err := db.Exec("INSERT INTO articles (title, timestamp, url, content) VALUES(?, ?, ?, ?);", article.Title, article.Timestamp.Unix(), url, blob)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
 
 func GetArticleByID(id int64) *document.Article {
+	db := acquire()
+	defer recycle(db)
+
 	// TODO: Cache
 
-	stmt, _ := db.Prepare("SELECT content FROM articles WHERE id = ?;")
-	result, _ := stmt.Query(id)
-	defer result.Close()
+	articles, _ := db.Query("SELECT content FROM articles WHERE id = ?;", id)
 
-	if result.Next() {
+	if articles.Next() {
 		var blob []byte
-		result.Scan(&blob)
+		articles.Scan(&blob)
 		article, _, _ := ToElement(blob)
 		return article.(*document.Article)
 	}
@@ -152,6 +185,9 @@ func GetArticleByID(id int64) *document.Article {
 }
 
 func GetLatestArticles() document.Content {
+	db := acquire()
+	defer recycle(db)
+
 	// TODO: Cache
 
 	content := make(document.Content, 0, 5)
--- storage/database_test.go
+++ storage/database_test.go
@@ -1,19 +1,41 @@
 package storage
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"sync"
+	"testing"
+)
+
+func init() {
+	f, _ := ioutil.TempFile(os.TempDir(), "gocms_db_test")
+	Startup(f.Name())
+}
 
 func TestDatabase(t *testing.T) {
 	t.Parallel()
-	Startup(":memory:")
 
 	if want, got := "Welcome to go.cms!", GetArticleByID(1).Title; want != got {
 		t.Error("ID->Title is incorrect: Want(", want, ") but Got(", got, ")")
 	}
 
-	if want, got := int64(1), GetArticleIDByURL("welcome-to-go-cms"); want != got {
-		t.Error("URL->Article ID is incorrect: Want(", want, ") but Got(", got, ")")
+	got, err := GetArticleIDByURL("welcome-to-go-cms")
+	if err != nil {
+		t.Error(err)
 	}
+	if 1 != got {
+		t.Error("URL->Article ID is incorrect: Want(", 1, ") but Got(", got, ")")
+	}
+}
 
-	// For testing, free up some resources as we won't be using the database apart from this one test.
-	db.Close()
+func BenchmarkExtremeConcurrency(b *testing.B) {
+	var wg sync.WaitGroup
+	for i := 0; i < b.N; i++ {
+		wg.Add(1)
+		go func() {
+			GetLatestArticles()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
